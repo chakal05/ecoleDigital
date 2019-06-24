d8e8b89c8e145622f896fbe93eb47f360864dc67
= <=>
2019-06-24 03:07:05 +0200

    test vscode

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-    $(document).ready(function(){
-    smoothScroll();
-    up();
-    })
+$(document).ready(function() {
+	smoothScroll();
+	up();
+});
 
-    // Smooth scroll navbar
-    function smoothScroll(){
-    $("nav a").click(function(e){
-    e.preventDefault();
-    let first = this.hash;
-    let target = first;
+// Smooth scroll navbar
+function smoothScroll() {
+	$('nav a').click(function(e) {
+		e.preventDefault();
+		let first = this.hash;
+		let target = first;
 
-    $("body, html").animate({
-    scrollTop: $(target).offset().top 
-    }, 2000)
+		$('body, html').animate(
+			{
+				scrollTop: $(target).offset().top
+			},
+			2000
+		);
+	});
 
-    })
+	// Smooth scroll for yellow button
+	$('.top .pres a').click(function(e) {
+		e.preventDefault();
+		var desc = $($(this).attr('href')).offset().top;
 
-    // Smooth scroll for yellow button
-    $(".top .pres a").click(function(e){
-    e.preventDefault();
-    var desc =
-    $($(this).attr("href")).offset().top;
+		$('body, html').animate(
+			{
+				scrollTop: desc
+			},
+			1500
+		);
+	});
+}
 
-    $("body, html").animate({
-    scrollTop: desc
-    }, 1500)
+//Positionning top text
+function up() {
+	$('.navbar-toggler ').click(function() {
+		$('.pres').css('margin-top', '-3%', down());
+	});
+}
 
-    });
+function down() {
+	$('.navbar-toggler').click(function() {
+		$('.pres').css('margin-top', '11%', up());
+	});
+}
 
-    }
-
-    //Positionning top text 
-    function up(){
-    $(".navbar-toggler ").click(function(){
-    $(".pres").css('margin-top','-3%', down());
-    })
-    }
-
-    function down(){
-    $(".navbar-toggler").click(function(){
-    $(".pres").css('margin-top','11%', up());
-    })
-    }
+// todo
